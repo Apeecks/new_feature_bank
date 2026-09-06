9c112e7603bfaf53afe965ab6d50c01e9e6549f7
Apeecks <apeecks@mail.ru>
2026-09-07 06:51:02 +0800

    fix docstring

--- src/processing.py
+++ src/processing.py
@@ -1,5 +1,23 @@
 def filter_by_state(unsorted_list: list, state: str = "EXECUTED") -> list:
-    """Сортировка по указанному ключу: state"""
+    """
+    Фильтрует список банковских операций по статусу.
+
+    Функция принимает список словарей с банковскими операциями
+    и возвращает новый список, содержащий только те операции,
+    у которых значение ключа "state" соответствует переданному
+    значению.
+    Если аргумент state не передан, используется значение
+    "EXECUTED".
+
+    Пример:
+        operations = [
+             {"id": 1, "state": "EXECUTED"},
+             {"id": 2, "state": "CANCELED"},
+        ]
+
+        filter_by_state(operations, "EXECUTED")
+        [{'id': 1, 'state': 'EXECUTED'}]
+    """
     list_sort = []
     for x in unsorted_list:
         if x.get("state") == state:
@@ -10,5 +28,23 @@ def filter_by_state(unsorted_list: list, state: str = "EXECUTED") -> list:
 
 
 def sort_by_date(unsorted_list: list, reverse: bool = True) -> list:
-    """Сортировка по дате"""
+    """
+    Сортирует список банковских операций по дате.
+
+    Функция принимает список словарей с банковскими операциями
+    и возвращает новый список, отсортированный по значению
+    ключа "date".
+
+    По умолчанию сортировка выполняется по убыванию,
+    то есть сначала идут более поздние операции.
+
+    Пример:
+        operations = [
+             {"id": 1, "date": "2018-06-30T02:08:58.425572"},
+             {"id": 2, "date": "2019-07-03T18:35:29.512364"},
+        ]
+
+        sort_by_date(operations)
+        [{'id': 2, 'date': '2019-07-03T18:35:29.512364'}, {'id': 1, 'date': '2018-06-30T02:08:58.425572'}]
+    """
     return sorted(unsorted_list, key=lambda x: x["date"], reverse=reverse)
